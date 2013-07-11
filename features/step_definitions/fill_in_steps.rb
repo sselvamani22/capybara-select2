ffb62df340c97302eba28ad68f8fcb250e6a48a6
Selvamani <sselvamani22@gmail.com>
2013-07-11 17:49:01 +0530

    Update fill_in_steps.rb

--- features/step_definitions/fill_in_steps.rb
+++ features/step_definitions/fill_in_steps.rb
@@ -1,17 +1,17 @@
-Given "I am on a page with a text input" do
+Given /^I am on a page with a text input$/ do
   visit '/'
 end
 
-When "I select value" do
-sleep 1
+When /^I select value$/ do
+  sleep 2
   select2("e9", "select", "Arizona",false)
-  sleep 1
+  sleep 2
   select2("e2", "2", "Delaware",true, true)
-select2("e2", "2", "Florida",true, true)
-  sleep 30
+  select2("e2", "2", "Florida",true, true)
+  sleep 10
 end
 
-Then "I should see value" do
+Then /^I should see value$/ do
   page.should have_content "Florida"
 end
 
